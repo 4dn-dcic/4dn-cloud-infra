d3d5ebe2656a447608741b34b94c37fc02a7253b
Eric Berg <Eric_Berg@hms.harvard.edu>
2020-11-12 13:21:00 -0500

    add comments + uncomment the rb --force

--- bin/delete_pending_buckets.sh
+++ bin/delete_pending_buckets.sh
@@ -2,14 +2,16 @@
 
 set -v -x
 
+# To test, uncomment the ls line, comment the rb --force line, and run
+# Outputs saved to the out/ gitignored dir
+
 for i in `cat pending_deletion`
 do
 echo "force deleting ${i}...press key to proceed or ctrl+c to cancel"
 read line
-echo "would delete here, ls first"
 echo "follow progress with:"
 echo "tail -f ../out/delete_log_${i}.log"
-#docker run --rm -ti -v ~/.aws:/root/.aws amazon/aws-cli s3 rb s3://${i} --force >> ../out/delete_log_${i}.log
-docker run --rm -ti -v ~/.aws:/root/.aws amazon/aws-cli s3 ls s3://${i} --summarize >> ../out/delete_log_${i}.log
+docker run --rm -ti -v ~/.aws:/root/.aws amazon/aws-cli s3 rb s3://${i} --force >> ../out/delete_log_${i}.log
+#docker run --rm -ti -v ~/.aws:/root/.aws amazon/aws-cli s3 ls s3://${i} --summarize >> ../out/delete_log_${i}.log
 done
 
