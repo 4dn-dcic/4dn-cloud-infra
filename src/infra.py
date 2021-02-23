191bbab2b5e220dfcf8109a43431f6d2f0468aee
Eric Berg <Eric_Berg@hms.harvard.edu>
2021-02-23 14:57:17 -0500

    disable beanstalk

--- src/infra.py
+++ src/infra.py
@@ -108,9 +108,9 @@ class C4Infra(C4Application):
         """ Add Beanstalk application to template self.t """
 
         # Adds application TODO iterate on with CI
-        self.t.add_resource(self.beanstalk_application())
-        self.t.add_resource(self.dev_beanstalk_environment())
-        self.t.add_resource(self.beanstalk_application_version())
+        # self.t.add_resource(self.beanstalk_application())
+        # self.t.add_resource(self.dev_beanstalk_environment())
+        # self.t.add_resource(self.beanstalk_application_version())
 
 
 class C4InfraTrial(C4Infra):
