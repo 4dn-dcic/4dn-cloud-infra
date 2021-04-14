64412c7edcbaa4813aa445196f950f9780f33f1d
Eric Berg <Eric_Berg@hms.harvard.edu>
2021-04-14 09:22:03 -0400

    initial test of chalice package: use a minimal app

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-from chalicelib.app import app
+#from chalicelib.app import app
+from chalice import Chalice
 
+# Minimal app.py; used to initially verify packaging scripts
+app = Chalice(app_name='foursight_cgap_trial')
+
+
+@app.route('/')
+def index():
+    return {'minimal': 'foursight_cgap_trial'}
