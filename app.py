20b3e358f600ce984061b1960954ce85f1e4d05e
Eric Berg <Eric_Berg@hms.harvard.edu>
2021-04-16 09:52:12 -0400

    add /view/{environ} route

--- app.py
+++ app.py
@@ -17,3 +17,12 @@ def index():
     return Response(status_code=302, body=json.dumps(resp_headers),
                     headers=resp_headers)
 
+
+@app.route('/view/{environ}', methods=['GET'])
+def view_route(environ):
+    """
+    Non-protected route
+    """
+    req_dict = app.current_request.to_dict()
+    domain, context = app_utils_obj.get_domain_and_context(req_dict)
+    return app_utils_obj.view_foursight(environ, app_utils_obj.check_authorization(req_dict, environ), domain, context)
