5cee0b4b4f90c663ead35baf0725084e50a91bfd
Eric Berg <Eric_Berg@hms.harvard.edu>
2021-04-19 08:42:56 -0400

    don't instantiate cgap app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import json
 import logging
-from chalicelib.app import DEFAULT_ENV
+# from chalicelib.app import DEFAULT_ENV
 from chalicelib.app_utils import AppUtils as AppUtils_from_cgap  # naming convention used in foursight-cgap
 # from chalicelib.vars import FOURSIGHT_PREFIX
 from chalice import Chalice, Response
 from os.path import dirname
 
-logging.basicConfig(encoding='utf8', level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 # Minimal app.py; used to initially verify packaging scripts
@@ -14,6 +14,7 @@ app = Chalice(app_name='foursight_cgap_trial')
 
 HOST = 'https://6kpcfpmbni.execute-api.us-east-1.amazonaws.com/api'
 FOURSIGHT_PREFIX = 'foursight-cgap-mastertest'
+DEFAULT_ENV = 'cgap'
 
 
 class AppUtils(AppUtils_from_cgap):
