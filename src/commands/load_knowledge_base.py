f2c1ea15a65ecb7b5fe24e9e686bce7b9f60e12a
Kent Pitman <kent_pitman@hms.harvard.edu>
2021-08-19 18:44:47 -0400

    Additional tweaks.

--- src/commands/load_knowledge_base.py
+++ src/commands/load_knowledge_base.py
@@ -6,16 +6,16 @@ import subprocess
 
 from dcicutils import ff_utils
 from dcicutils.command_utils import y_or_n
-from dcicutils.lang_utils import conjoined_list
+from dcicutils.lang_utils import conjoined_list, n_of
 from dcicutils.misc_utils import PRINT
 from tqdm import tqdm
-from ..base import ConfigManager, ENV_NAME as DEFAULT_ENV_NAME
+from ..base import ENV_NAME as DEFAULT_ENV_NAME
 from ..parts.ecs import C4ECSApplicationExports
 
 
 EPILOG = __doc__
 
-DEFAULT_PATH_TO_CREDS = os.path.expanduser('~/.cgap-keys.json')
+CREDS_PATH = os.path.expanduser('~/.cgap-keys.json')
 KNOWLEDGE_BASE_DIR = 'test_data/knowledge_base'
 KB_INSERTS_FILE = 'temp-local-inserts'
 KB_INSERTS_DIR = os.path.join(KNOWLEDGE_BASE_DIR, KB_INSERTS_FILE)
@@ -29,28 +29,25 @@ BASES = {
     'higlass': HIGLASS_BASE,
     'genes': GENES_BASE,
 }
+DEFAULT_INCLUDE_BASES = 'higlass'
 
-DEFAULT_CURRENT_BASE = 'higlass'
 
-
-def load_knowledge_base(env_name=None,
-                        path_to_creds=None,
-                        include_bases=None,
-                        # current_base=None,
-                        # include_genes=False,
-                        confirm=True):
+def load_knowledge_base(env_name=None, include_bases=None, confirm=True):
     """ Must be invoked from top level - reads the 3 knowledge base files (provided in zip). """
 
+    include_bases = DEFAULT_INCLUDE_BASES if include_bases is None else include_bases
     env_name = env_name or DEFAULT_ENV_NAME
     # Precondition: both of these must exist
-    path_to_creds = path_to_creds or DEFAULT_PATH_TO_CREDS
     # SERVER = 'http://c4ecstrialalphacgapmastertest-273357903.us-east-1.elb.amazonaws.com'
     if include_bases == 'all':
         include_bases = ",".join(BASES.keys())
-    print(f"Including data for {conjoined_list(include_bases.split(','))}.")
-    current_base = sum([BASES[key] for key in include_bases.split(',')], [])
-    print(f"Loading {conjoined_list(current_base)}.")
-
+    if not include_bases:
+        PRINT("Nothing to load.")
+        return
+    included_bases = include_bases.split(",")
+    print(f"Including data for {conjoined_list(included_bases)}.")
+    included_files = sum([BASES[key] for key in included_bases], [])
+    print(f"{n_of(included_files, 'file')} will be loaded: {conjoined_list(included_files)}.")
     application_url = C4ECSApplicationExports.get_application_url(env_name)
     if confirm and not y_or_n(f"Load data to {env_name} server {application_url}", default=True):
         PRINT("Aborted.")
@@ -66,7 +63,7 @@ def load_knowledge_base(env_name=None,
         PRINT(f"Missing inserts path: {KB_INSERTS_DIR}")
         return
 
-    with io.open(path_to_creds) as keyfile:
+    with io.open(CREDS_PATH) as keyfile:
         creds = json.load(keyfile)
 
     keys = None
@@ -78,7 +75,7 @@ def load_knowledge_base(env_name=None,
     if keys is None:
         raise Exception('Did not locate specified server, check creds file.')
 
-    for file in current_base:
+    for file in included_files:
         with io.open('/'.join([KB_INSERTS_DIR, file])) as collection_meta:
             items = json.load(collection_meta)
             for item in tqdm(items):
@@ -94,9 +91,9 @@ def main(simulated_args=None):
         epilog=EPILOG,  formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     parser.add_argument("--env_name", default=None, help="name of environment to load args into")
-    parser.add_argument("--include-bases", default=DEFAULT_CURRENT_BASE, help="name of environment to load args into")
-    parser.add_argument("--no-confirm", default=False, action="store_true", help="whether to include genes",
-                        dest="no_confirm")
+    parser.add_argument("--include-bases", default=DEFAULT_INCLUDE_BASES, help="name of environment to load args into")
+    parser.add_argument("--no-confirm", dest="no_confirm", default=False, action="store_true",
+                        help="whether to include genes")
     args = parser.parse_args(args=simulated_args)
 
     load_knowledge_base(env_name=args.env_name, confirm=not args.no_confirm, include_bases=args.include_bases)
