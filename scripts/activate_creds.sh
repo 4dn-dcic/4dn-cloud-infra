ffedae1c48ef8c4c4dcaf599fa5465a754baf2b3
Will Ronchetti <wrr33@cornell.edu>
2022-03-02 15:51:50 -0500

    Update scripts/activate_creds.sh
    Co-authored-by: Kent M Pitman <netsettler@users.noreply.github.com>

--- scripts/activate_creds.sh
+++ scripts/activate_creds.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-rm custom 
+if [ -L custom ]; then
+    echo "A symbolic link exists for file 'custom'. It will be redefined."
+    ls -dal custom
+    rm custom
+elif [ -e custom ]; then
+    echo "File 'custom' exists but is not a symbolic link. You must fix this manually."
+    exit 1
+fi
 
 ln -s $1 custom
 
