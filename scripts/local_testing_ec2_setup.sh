d802d5c7103855aa595f22680e4fcad36487dfdd
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2022-04-01 11:11:24 -0400

    Source poetry env

--- scripts/local_testing_ec2_setup.sh
+++ scripts/local_testing_ec2_setup.sh
@@ -10,6 +10,7 @@ sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
 	libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
 	libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
 
+
 curl https://pyenv.run | bash
 
 export PATH="$HOME/.pyenv/bin:$PATH"
@@ -23,11 +24,9 @@ sed -i '1i export PATH="$HOME/.pyenv/bin:$PATH"' ~/.bashrc
 pyenv install 3.7.12
 pyenv global 3.7.12
 pyenv virtualenv 3.7.12 foursight-testing
-cd 4dn-cloud-infra
-cd $HOME
-
 
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+source $HOME/.poetry/env
 sed -i '1i export PATH="$HOME/.poetry/bin:$PATH"' ~/.bashrc
 
 git clone https://github.com/dbmi-bgm/foursight-cgap
