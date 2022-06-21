0aa9597f8813956dfe30102a81d1da763b6dee62
David Michaels <dmichaels@gmail.com>
2022-06-21 14:30:19 -0400

    Initial cut at setup-remaining-secrets script, and some associated refactoring.

--- src/constants.py
+++ src/constants.py
@@ -122,6 +122,27 @@ class C4DatastoreBase:
     STACK_NAME_TOKEN = "datastore"
     STACK_TITLE_TOKEN = "Datastore"
     APPLICATION_CONFIGURATION_SECRET_NAME_SUFFIX = 'ApplicationConfiguration'
+    RDS_SECRET_NAME_SUFFIX = 'RDSSecret'  # Used as logical id suffix in resource names
+
+# dmichaels/2022-06-22: Factored out from StackNameMixin in part.py.
+class StackNameMixinBase:
+    SHARING = 'env'
+    def SHARING_QUALIFIERS(env_name: str, ecosystem: str):
+        _SHARING_QUALIFIERS = {
+            'env': f"{env_name}",
+            'ecosystem': f"{ecosystem}",
+            'account': "",
+        }
+        return _SHARING_QUALIFIERS
+
+# dmichaels/2022-06-22: Factored out from C4IAM in iam.py.
+class C4IAMBase:
+    """
+    Factored out of C4IAM to to generate names before orchestration (e.g. setup-remaining-secrets).
+    """
+    STACK_NAME_TOKEN = "iam"
+    STACK_TITLE_TOKEN = "IAM"
+    SHARING = 'ecosystem'
 
 
 # dmichaels/2022-07-05: Factored out from sentieon.py.
--- src/names.py
+++ src/names.py
@@ -1,11 +1,13 @@
 from dcicutils.cloudformation_utils import camelize
-
+from dcicutils.exceptions import InvalidParameterError
 from .c4name import C4Name
 from .constants import (
     COMMON_STACK_PREFIX,
     COMMON_STACK_PREFIX_CAMEL_CASE,
     C4DatastoreBase,
-    C4SentieonSupportBase
+    C4IAMBase,
+    C4SentieonSupportBase,
+    StackNameMixinBase
 )
 
 
@@ -30,6 +32,35 @@ class Names:
             qualifier = env_name
             c4name = cls.suggest_stack_name(title_token, name_token, qualifier)
         return c4name.logical_id(camelize(env_name) + C4DatastoreBase.APPLICATION_CONFIGURATION_SECRET_NAME_SUFFIX)
+
+    # dmichaels/2022-06-20: Factored out from C4Datastore.rds_secret_logical_id() in datastore.py.
+    @classmethod
+    def rds_secret_logical_id(cls, env_name: str, c4name: C4Name = None) -> str:
+        if not c4name:
+            title_token = C4DatastoreBase.STACK_TITLE_TOKEN  # Datastore (in constants.py, from C4Datastore.STACK_TITLE_TOKEN)
+            name_token = C4DatastoreBase.STACK_NAME_TOKEN    # datastore (in constants.py, from C4Datastore.STACK_NAME_TOKEN)
+            qualifier = env_name
+            c4name = cls.suggest_stack_name(title_token, name_token, qualifier)
+        return c4name.logical_id(camelize(env_name) + C4DatastoreBase.RDS_SECRET_NAME_SUFFIX, context='rds_secret_logical_id')
+
+    # dmichaels/2022-06-22: Factored out from C4IAM.suggest_sharing_qualifier() in part.py.
+    @classmethod
+    def suggest_sharing_qualifier(cls, sharing: str, env_name: str, ecosystem: str) -> str:
+        sharing_qualifiers = StackNameMixinBase.SHARING_QUALIFIERS(env_name, ecosystem)
+        if sharing not in sharing_qualifiers:
+            raise InvalidParameterError(parameter=f'{cls}.SHARING', value=sharing,
+                                        options=list(sharing_qualifiers.keys()))
+        return sharing_qualifiers[sharing]
+
+    # dmichaels/2022-06-22: Factored out from C4IAM.ecs_s3_iam_user() in iam.py.
+    @classmethod
+    def ecs_s3_iam_user_logical_id(cls, c4name: C4Name = None, env_name: str = None, ecosystem: str = None) -> str:
+        if not c4name:
+            title_token = C4IAMBase.STACK_TITLE_TOKEN  # Datastore (in constants.py, from C4Datastore.STACK_TITLE_TOKEN)
+            name_token = C4IAMBase.STACK_NAME_TOKEN    # datastore (in constants.py, from C4Datastore.STACK_NAME_TOKEN)
+            qualifier = cls.suggest_sharing_qualifier(C4IAMBase.SHARING, env_name, ecosystem)
+            c4name = cls.suggest_stack_name(title_token, name_token, qualifier)
+        return c4name.logical_id('ApplicationS3Federator')
 
     # dmichaels/2022-07-05: Created to get Sentieon stack name; equivalent to
     # C4SentieonSupport.suggest_stack_name() but without importing sentieon.py which pulls in
