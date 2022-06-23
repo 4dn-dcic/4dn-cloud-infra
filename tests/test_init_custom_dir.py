c7a2247953a213417219afd825429a6c0174c49a
David Michaels <dmichaels@gmail.com>
2022-06-23 12:58:15 -0400

    Test updates for latest refactoring.

--- tests/test_init_custom_dir.py
+++ tests/test_init_custom_dir.py
@@ -8,10 +8,10 @@ import stat
 import tempfile
 from typing import Callable
 from contextlib import contextmanager
-from src.auto.init_custom_dir.cli import (get_fallback_identity, main)
-from src.auto.init_custom_dir.defs import InfraDirectories, InfraFiles
-from src.auto.utils.misc_utils import obfuscate
 from dcicutils.qa_utils import printed_output as mock_print
+from src.auto.init_custom_dir.cli import (get_fallback_identity, main)
+from src.auto.utils.locations import InfraDirectories, InfraFiles
+from src.auto.utils.misc_utils import obfuscate
 
 # Tests for the main init-custom-dir script.
 
