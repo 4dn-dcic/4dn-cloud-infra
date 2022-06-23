8bb7576397ee47098de86a70198184f42c42244e
David Michaels <dmichaels@gmail.com>
2022-06-23 10:06:24 -0400

    Mostly docstring updates.

--- src/auto/setup_remaining_secrets/utils.py
+++ src/auto/setup_remaining_secrets/utils.py
@@ -2,7 +2,7 @@ from prettytable import PrettyTable
 import re
 from dcicutils.misc_utils import PRINT
 # TODO: Probably should factor out utils from init_custom_dir into common "auto" utils.
-from ..init_custom_dir.utils import (exit_with_no_action, obfuscate)
+from ..init_custom_dir.utils import (exit_with_no_action, obfuscate, setup_and_action)
 
 
 def should_obfuscate(key: str) -> bool:
@@ -24,7 +24,8 @@ def should_obfuscate(key: str) -> bool:
     return any(regex.match(key) for regex in secret_key_names_regex)
 
 
-def print_dictionary_as_table(header_name: str, header_value: str, dictionary: dict, display_value, sort: bool = True) -> None:
+def print_dictionary_as_table(header_name: str, header_value: str,
+                              dictionary: dict, display_value: callable, sort: bool = True) -> None:
     table = PrettyTable()
     table.field_names = [header_name, header_value]
     table.align[header_name] = "l"
