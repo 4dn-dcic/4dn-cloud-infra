65704e94d340cbb93eab18c573c661d3903202a5
David Michaels <dmichaels@gmail.com>
2022-07-08 08:17:32 -0400

    Various changes from (Kent) PR feedback.

--- src/auto/utils/args_utils.py
+++ src/auto/utils/args_utils.py
@@ -10,24 +10,18 @@ def add_aws_credentials_args(args_parser: argparse.ArgumentParser) -> None:
     :param args_parser: The argparse.ArgumentParser to add arguments to.
     """
     args_parser.add_argument("--aws-access-key-id", required=False,
-                             dest="aws_access_key_id",
                              help=f"Your AWS access key ID; also requires --aws-access-secret-key.")
     args_parser.add_argument("--aws-credentials-dir", required=False,
-                             dest="aws_credentials_dir",
                              help=f"Alternate full path to your custom AWS credentials directory.")
     args_parser.add_argument("--aws-credentials-name", required=False,
-                             dest="aws_credentials_name",
                              help=f"The name of your AWS credentials,"
                                   f"e.g. <aws-credentials-name>"
                                   f" from {InfraDirectories.AWS_DIR}.<aws-credentials-name>.")
     args_parser.add_argument("--aws-region", required=False,
-                             dest="aws_region",
                              help="The AWS region.")
     args_parser.add_argument("--aws-secret-access-key", required=False,
-                             dest="aws_secret_access_key",
                              help=f"Your AWS access key ID; also requires --aws-access-key-id.")
     args_parser.add_argument("--aws-session-token", required=False,
-                             dest="aws_session_token",
                              help=f"Your AWS session token.")
 
 
@@ -35,3 +29,5 @@ def validate_aws_credentials_args(args) -> None:
     if ((args.aws_access_key_id or args.aws_secret_access_key)
        and not (args.aws_access_key_id and args.aws_secret_access_key)):
         exit_with_no_action("Either none or both --aws-access-key-id and --aws-secret-access-key must be specified.")
+    if args.aws_access_key_id and args.aws_credentials_dir:
+        exit_with_no_action("Cannot specify both --aws-credentials-dir and --aws-access-key-id/--aws-secret-access-key.")
