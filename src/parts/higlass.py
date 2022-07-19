c51b16799b77de20e48ff428630cab97440555c8
William Ronchetti <wrr33@cornell.edu>
2022-07-19 11:12:48 -0400

    add y

--- src/parts/higlass.py
+++ src/parts/higlass.py
@@ -63,7 +63,7 @@ class C4HiglassServer(C4EC2Common):
         return [
             '#!/bin/bash -xe', '\n',
             'sudo apt-get update', '\n',
-            'sudo apt-get install apt-transport-https ca-certificates curl software-properties-common git', '\n',
+            'sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common git', '\n',
             'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -', '\n',
             'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"', '\n',
             'sudo apt update; apt-cache policy docker-ce', '\n'
