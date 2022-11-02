ced66a67abeb2ee69ff1a282a90d656b642acf11
David Michaels <david_michaels@hms.harvard.edu>
2022-11-02 12:44:24 -0400

    Comments.

--- app.py
+++ app.py
@@ -17,16 +17,30 @@ def _is_foursight_fourfront():
         return EnvUtils.app_case(if_cgap=False, if_fourfront=True)
 
 
-# TODO: Better way to communicate this to foursight-cgap (chalicelib_cgap) or
-# foursight (chalicelib_fourfront)? If this is set there then use it as the check_setup.json
-# directory, otherwise use the local (chalicelib_cgap or chalicelib_fourfront) directory.
-os.environ["FOURSIGHT_CHECK_SETUP_DIR"] = os.path.dirname(__file__)
+# TODO: Better way to communicate the directory containing the check_setup.json
+# file to foursight-cgap (chalicelib_cgap) or foursight (chalicelib_fourfront)?
+#
+# If this FOURSIGHT_CHECK_SETUP_DIR environment variable is set there then use it as the
+# check_setup.json directory (but only if it contains a non-empty check_setup.json file);
+# otherwise use the local directory chalicelib_cgap or chalicelib_fourfront in foursight-cgap
+# or foursight, respectively.
+#
+# The check_setup.json file here (in 4dn-cloud-infra) gets there by virtue of the pyproject
+# resolve-foursight-checks command, which now BTW takes a --app argument to indicate if the
+# file should be for Foursight-CGAP or Foursight-Fourfront. This command pulls check_setup.json
+# from either the chalicelib_cgap or chalicelib_fourfront directories of foursight-cgap or
+# fourfront, respectively; and it expands any <env-name> placeholders within it to the given
+# environment name. However, this expansion is now also done at runtime by Foursight, using
+# the default environment name, so running resolve-foursight-checks is now not strictly necesssary.
+
+if not os.environ.get("FOURSIGHT_CHECK_SETUP_DIR", None):
+    os.environ["FOURSIGHT_CHECK_SETUP_DIR"] = os.path.dirname(__file__)
 
 # Note that foursight-cgap (chalicelib_cgap) and foursight (chalicelib_fourfrount) now
 # live side-by-side; no longer (as of 2022-10-30) both packaging to chalicelib; nothing
 # substantively magic about that name afterall. But this app.py file name IS magic (for
 # Chalice) which is why it is here in 4dn-cloud-infra and pulls in either foursight-cgap
-# or foursight (fourfront) depending on the is_foursight_fourfront function
+# or foursight (fourfront) depending on the _is_foursight_fourfront function above.
 if _is_foursight_fourfront():
     PRINT("Foursight-Fourfront: Including app_utils and check_schedules from chalicelib_fourfront.")
     from chalicelib_fourfront.app_utils import AppUtils
