73394f0a3df31025e9648d69b943bb2de87a2c9c
William Ronchetti <wrr33@cornell.edu>
2022-12-01 13:48:53 -0500

    lock foursight-cgap, fix autoscaling bug

--- src/parts/ecs.py
+++ src/parts/ecs.py
@@ -528,6 +528,9 @@ class C4ECSApplication(C4Part):
             Allows passing a deployment_type (for use with blue/green).
             In this case deployment_type == '', 'blue' or 'green'
         """
+        queue_name = (ConfigManager.get_config_setting(Settings.ENV_NAME) +
+                      f'-{deployment_type}-secondary-indexer-queue' if deployment_type else
+                      ConfigManager.get_config_setting(Settings.ENV_NAME) + f'-secondary-indexer-queue')
         return Alarm(
             f'IndexingQueueDepthAlarm{deployment_type}',
             AlarmDescription='Alarm if total queue depth exceeds %s' % depth,
@@ -536,8 +539,7 @@ class C4ECSApplication(C4Part):
             Dimensions=[
                 MetricDimension(
                     Name='QueueName',
-                    Value=ConfigManager.get_config_setting(Settings.ENV_NAME) +
-                    f'-{deployment_type}-secondary-indexer-queue'),
+                    Value=queue_name,
             ],
             Statistic='Maximum',
             Period='300',
