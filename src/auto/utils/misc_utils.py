b147deccf82176384159bc717e254d70ee647112
David Michaels <david_michaels@hms.harvard.edu>
2023-02-27 09:59:11 -0500

    Fix unit test.

--- src/auto/utils/misc_utils.py
+++ src/auto/utils/misc_utils.py
@@ -29,7 +29,7 @@ def get_json_config_file_value(name: str, config_file: str, fallback: str = None
     try:
         with io.open(config_file, "r") as config_fp:
             config_json = json.load(config_fp)
-            value = str(config_json.get(name))
+            value = config_json.get(name)
             return value if value else fallback
     except Exception:
         return fallback
