1180285255a897d3ec1a461260389dcf1d878d08
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2023-04-26 14:00:50 -0400

    Update magma version

--- scripts/foursight_development_ec2_setup.sh
+++ scripts/foursight_development_ec2_setup.sh
@@ -23,7 +23,6 @@ sudo apt-get update
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
 	libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
 	libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
-export VARIABLE="${VARIABLE:='foo'}"
 
 if [ -z $current_pyenv ]; then
     git clone https://github.com/pyenv/pyenv.git ~/.pyenv
