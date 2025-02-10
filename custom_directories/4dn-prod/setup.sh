1f2b20186cb37704e0592871f3a2f74afaac8d96
David Michaels <david_michaels@hms.harvard.edu>
2025-02-10 08:57:01 -0500

    added custom_directories directory

--- custom_directories/4dn-prod/setup.sh
+++ custom_directories/4dn-prod/setup.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-THIS_SETUP_DIR=`realpath $(dirname $BASH_SOURCE)`
-THIS_CUSTOM_DIR=$THIS_SETUP_DIR/custom
-YOUR_CUSTOM_DIR=$THIS_SETUP_DIR/../../custom
-
-IDENTITY=FoursightProductionApplicationConfiguration
-AUTH0_CLIENT=`aws-get-secret ${IDENTITY}/ENCODED_AUTH0_CLIENT`
-AUTH0_SECRET=`aws-get-secret ${IDENTITY}/ENCODED_AUTH0_SECRET`
-S3_ENCRYPT_KEY=`aws-get-secret ${IDENTITY}/S3_ENCRYPT_KEY`
-
-rm -rf $THIS_CUSTOM_DIR
-mkdir -p $THIS_CUSTOM_DIR
-
-cp $THIS_SETUP_DIR/template.config.json $THIS_CUSTOM_DIR/config.json
-
-sed -e "s/\${AUTH0_CLIENT}/$AUTH0_CLIENT/ ; s/\${AUTH0_SECRET}/$AUTH0_SECRET/" $THIS_SETUP_DIR/template.secrets.json > $THIS_CUSTOM_DIR/secrets.json
-chmod 400 $THIS_CUSTOM_DIR/secrets.json
-
-echo $S3_ENCRYPT_KEY > $THIS_CUSTOM_DIR/s3_encrypt_key.txt
-chmod 400 $THIS_CUSTOM_DIR/s3_encrypt_key.txt
-
-ln -s ~/.aws $THIS_CUSTOM_DIR/aws_creds
-
-rm -f $YOUR_CUSTOM_DIR
-ln -s $THIS_SETUP_DIR/custom $YOUR_CUSTOM_DIR
+$DIR/../setup.sh 4dn-prod
---
+++ custom_directories/setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if [[ $# -ne 1 ]]; then
+    echo "usage: $0 custom_directories_sub_directory_name"
+    exit 1
+fi
+
+ENV=$1
+THIS_SETUP_DIR=$DIR/$ENV
+
+if [[ -d "$THIS_SETUP_DIR" ]]; then
+    echo "Setting up for: $ENV"
+else
+    echo "sub-directory not found: $THIS_SETUP_DIR"
+    exit 1
+fi
+
+THIS_CUSTOM_DIR=$THIS_SETUP_DIR/custom
+YOUR_CUSTOM_DIR=$THIS_SETUP_DIR/../../custom
+
+IDENTITY=FoursightProductionApplicationConfiguration
+AUTH0_CLIENT=`aws-get-secret ${IDENTITY}/ENCODED_AUTH0_CLIENT`
+AUTH0_SECRET=`aws-get-secret ${IDENTITY}/ENCODED_AUTH0_SECRET`
+S3_ENCRYPT_KEY=`aws-get-secret ${IDENTITY}/S3_ENCRYPT_KEY`
+
+rm -rf $THIS_CUSTOM_DIR ; mkdir -p $THIS_CUSTOM_DIR
+
+cp $THIS_SETUP_DIR/template.config.json $THIS_CUSTOM_DIR/config.json
+
+sed -e "s/\${AUTH0_CLIENT}/$AUTH0_CLIENT/ ; s/\${AUTH0_SECRET}/$AUTH0_SECRET/" $THIS_SETUP_DIR/template.secrets.json > $THIS_CUSTOM_DIR/secrets.json
+chmod 400 $THIS_CUSTOM_DIR/secrets.json
+
+echo $S3_ENCRYPT_KEY > $THIS_CUSTOM_DIR/s3_encrypt_key.txt
+chmod 400 $THIS_CUSTOM_DIR/s3_encrypt_key.txt
+
+ln -s ~/.aws $THIS_CUSTOM_DIR/aws_creds
+
+rm -f $YOUR_CUSTOM_DIR
+ln -s $THIS_SETUP_DIR/custom $YOUR_CUSTOM_DIR
