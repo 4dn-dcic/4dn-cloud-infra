d9c9e65c799e1e24f56c6e6fdc5aeed1df9ba68e
David Michaels <david_michaels@hms.harvard.edu>
2025-03-14 11:23:15 -0400

    script for foursight deploy

--- custom_directories/smaht-prod/setup.sh
+++ custom_directories/smaht-prod/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-$DIR/../setup.sh smaht-prod C4AppConfigFoursightSmahtProduction
+echo "TODO: Make sure your current/valid AWS environment is for: smaht-prod"
+IDENTITY=C4AppConfigFoursightSmahtProduction
+$DIR/../setup.sh smaht-prod $IDENTITY
+resolve-foursight-checks --env_name data --app smaht
+echo "TO DEPLOY FOURSIGHT RUN: poetry run cli provision foursight-smaht --upload-change-set --stage prod --foursight-identity $IDENTITY"
--- custom_directories/smaht-wolf/setup.sh
+++ custom_directories/smaht-wolf/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-$DIR/../setup.sh smaht-wolf C4AppConfigFoursightSmahtDevelopment
+echo "TODO: Make sure your current/valid AWS environment is for: smaht-wolf"
+IDENTITY=C4AppConfigFoursightSmahtDevelopment
+$DIR/../setup.sh smaht-wolf $IDENTITY
+resolve-foursight-checks --env_name smaht-wolf --app smaht
+echo "TO DEPLOY FOURSIGHT RUN: poetry run cli provision foursight-smaht --upload-change-set --stage prod --foursight-identity $IDENTITY"
